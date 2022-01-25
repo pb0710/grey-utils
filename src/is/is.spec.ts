48cafeb5b43866f7fea877415bb72be2e5733850
gray <1350210100@qq.com>
2022-01-25 10:41:42 +0800

    feat: is.basic type

--- src/is/is.spec.ts
+++ src/is/is.spec.ts
@@ -1,5 +1,32 @@
 import is from '.'
 
+describe('basic', () => {
+	it('undefined is undefined', () => {
+		expect(is.undefined(undefined)).toBe(true)
+	})
+	it('null is null', () => {
+		expect(is.null(null)).toBe(true)
+	})
+	it('false is boolean', () => {
+		expect(is.boolean(false)).toBe(true)
+	})
+	it('"abcd" is string', () => {
+		expect(is.string('abcd')).toBe(true)
+	})
+	it('function is function', () => {
+		expect(is.function((a: number, b: number) => a + b)).toBe(true)
+	})
+})
+
+describe('array', () => {
+	it('[] is array', () => {
+		expect(is.array([])).toBe(true)
+	})
+	it('{} is not array', () => {
+		expect(is.array({})).toBe(false)
+	})
+})
+
 describe('object', () => {
 	it('null is not object', () => {
 		expect(is.object(null)).toBe(false)
@@ -15,20 +42,5 @@ describe('object', () => {
 	})
 	it('Object is object', () => {
 		expect(is.object(Object.create(null))).toBe(true)
-	})
-})
-
-describe('function', () => {
-	it('function is function', () => {
-		expect(is.function((a: number, b: number) => a + b)).toBe(true)
-	})
-})
-
-describe('array', () => {
-	it('[] is array', () => {
-		expect(is.array([])).toBe(true)
-	})
-	it('{} is not array', () => {
-		expect(is.array({})).toBe(false)
 	})
 })
